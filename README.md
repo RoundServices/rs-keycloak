c1e25102922b8d3500aa0852876b067e83999d22
ggallard <ggallard@identicum.com>
2021-09-06 01:12:04 -0300

    Initial upload, split from rs-partners

---
+++ README.md
@@ -0,0 +1,10 @@
+# rs-keycloak
+
+## Pre-requisites
+- Python > 3.x
+- [rs-utils](https://github.com/RoundServices/rs-utils)
+
+## Deploy
+```sh
+python3 -m pip install --upgrade git+https://github.com/RoundServices/rs-keycloak.git@main
+```
---
+++ setup.py
@@ -0,0 +1,21 @@
+# rs-utils is available under the MIT License. https://github.com/roundservices/rs-utils/
+# Copyright (c) 2021, Round Services LLC - https://roundservices.biz/
+#
+# Author: Gustavo J Gallardo - ggallard@roundservices.biz
+#
+
+from setuptools import setup
+
+setup(
+    name='rs-keycloak',
+    version='1.0.0',
+    description='Python utilities for Keycloak',
+    url='git@github.com:RoundServices/rs-keycloak.git',
+    author='Round Services',
+    author_email='ggallard@roundservices.biz',
+    license='MIT License',
+    install_requires=['python-keycloak'],
+    packages=['rs.keycloak'],
+    zip_safe=False,
+    python_requires='>=3.0'
+)
