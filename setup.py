02f1741b5723bd1451515a80b78a38908228ccf7
Gustavo J Gallardo <ggallard@identicum.com>
2022-06-13 16:18:45 -0300

    temporary fixed to v0.27.0

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     author='Round Services',
     author_email='ggallard@roundservices.biz',
     license='MIT License',
-    install_requires=['python-keycloak>=0.27.0'],
+    install_requires=['python-keycloak=0.27.0'],
     packages=['rs.keycloak'],
     zip_safe=False,
     python_requires='>=3.0'
