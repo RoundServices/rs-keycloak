84478b46411779d9c3b640feb138a70fc29fd92e
ggallard <ggallard@identicum.com>
2023-03-31 20:48:38 -0300

    adjust version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup
 
 setup(
     name='rs-keycloak',
-    version='1.0.20221123',
+    version='1.0.20230331',
     description='Python utilities for Keycloak',
     url='git@github.com:RoundServices/rs-keycloak.git',
     author='Round Services',
